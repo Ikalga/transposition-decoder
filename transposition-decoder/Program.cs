42e49725ad5b0c794454f12607f2f5990bd8d083
Ikalga <a.ikaluga.06+github@gmail.com>
2015-09-28 23:56:04 +0900

    ユニコード文字コード→日本語変換まで実装

--- transposition-decoder/Program.cs
+++ transposition-decoder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,26 @@ namespace transposition_decoder
     {
         static void Main(string[] args)
         {
-            //TODO 作ったやつコミットする
-            Console.WriteLine("Hello,world !");
+            // ファイル名を受け取る
+            Console.WriteLine("デコード対象ファイルパスを入力: ");
+            string filePath = Console.ReadLine();
+            StreamReader r = new StreamReader(filePath);
+            string line;
+            string str = "";
+            while ((line = r.ReadLine()) != null) // 1行ずつ読み出し。
+            {
+                // ユニコード変換
+                for (int i = 0; i*4 < line.Length; i++)
+                {
+                    string charCode = line.Substring(i*4, 4);
+                    int charCode16 = Convert.ToInt32(charCode, 16);
+                    char c = Convert.ToChar(charCode16);
+                    str += c;
+                }
+            }
+            Console.WriteLine(str);
+
+            // 転置
             Console.ReadKey();
         }
     }
